725ee64af53b3de758ae571bf0c5bf25a818ecce
Manchi-Sravanthi <sravanthimanchi5@gmail.com>
2026-07-18 10:46:12 +0530

    Add files via upload

---
+++ wipro_TNP_practice/milestone2/threadcontrolandpriorities/Q1.java
@@ -0,0 +1,29 @@
+package multithreading.threadsynchronization;
+class NumberThread extends Thread {
+
+    public void run() {
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(i);
+
+            if (i == 5) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+}
+
+public class Q1 {
+
+    public static void main(String[] args) {
+
+        NumberThread t = new NumberThread();
+        t.start();
+
+    }
+}
